7c0779f75c6548cadbe790cf0d098437872a7d2d
Kirezi Cyisa Fidele <fihacker000@gmail.com>
2020-02-05 00:57:17 +0300

    Update wsgi.py

--- Makas/wsgi.py
+++ Makas/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Makas.settings')
