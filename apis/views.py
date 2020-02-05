6bb00c10b1408cb46dda7bc7ffce55c00b5cccda
fidele000 <fihacker000@gmail.com>
2020-02-05 10:04:48 +0300

    update

--- apis/views.py
+++ apis/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from bs4 import BeautifulSoup
+import urllib
 # Create your views here.
 
 def index(request):
